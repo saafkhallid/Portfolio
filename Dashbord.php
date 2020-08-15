f464a1623f0413cf27d6158afac641c94396a299
khalid saaf <khallid.saaf@gmail.com>
2020-08-15 21:12:13 +0100

    crier page administration

---
+++ Dashbord.php
@@ -0,0 +1,64 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administration</title>
+	
+ 
+
+     
+
+     
+    <link rel="stylesheet" href="css/Teampage.css">
+</head>
+<body>
+    
+<header>
+		<div class="logo">team<span>page</span></div>
+	</header>
+	<div class="nav-btn">Menu</div>
+	<div class="container">
+		
+		<div class="sidebar">
+			<nav>
+				<a href="#" class="">Administration<span>page</span></a>
+				<ul>
+					<li class="active" ><a href="#">Dashboard</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">contact</a></li>
+					<li><a href="#">accuil</a></li>
+					<li class=""><a href="#">login</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="main-content">
+			<h1> Welcome page Administration</h1>
+			
+      <div class="">
+				
+				
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+<!-- <?php
+
+
+
+
+
+
+?> -->
+
+
+</body>
+</html>
