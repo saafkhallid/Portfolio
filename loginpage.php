8902b8dbd5ecbf5abfc4f461b39e05d19413e449
khalid saaf <khallid.saaf@gmail.com>
2020-09-01 01:01:03 +0100

    fin page

--- loginpage.php
+++ loginpage.php
@@ -22,9 +22,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administration</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-	<!-- <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style> -->
+
  
 
      
